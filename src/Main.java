e3deff3662c7b0dbeca00439437365c4cc359a8f
Pavel Shustov <pavel.shustov777@gmail.com>
2024-10-20 16:41:46 +0300

    Enabled Fork/join

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,49 @@
 import java.io.File;
 import java.util.Set;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
 
-        MyThread thread = new MyThread(1);
+        /*MyThread thread = new MyThread(1);
         MyThread thread2 = new MyThread(2);
 
         thread.start();
         thread2.start();
 
+        */
+
         String folderPath = "D:/Музыка";
         File file = new File(folderPath);
 
         System.out.println(file.length());
-        System.out.println(); //Разделительная строка.
+       /* System.out.println(); //Разделительная строка.
 
-      /*  Set keys = System.getProperties().keySet();
+        Set keys = System.getProperties().keySet();
         for (Object key : keys) {
             System.out.println(key);
         }
         System.out.println(); //Разделительная строка.
         System.out.println(System.getProperties().get("user.dir"));
         */
+
+        long start = System.currentTimeMillis();
+
         System.out.println(); //Разделительная строка.
         System.out.println(getFolderSize(file));
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(); //Разделительная строка.
+        System.out.println(duration + "ms");
+
+        long start1 = System.currentTimeMillis();
+
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+        long duration1 = System.currentTimeMillis() - start1;
+        System.out.println(); //Разделительная строка.
+        System.out.println(duration1 + "ms");
     }
 
     public static long getFolderSize(File folder) {
