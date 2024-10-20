fd88fa4f6dee03793158b9978196bdc5d475a603
Pavel Shustov <pavel.shustov777@gmail.com>
2024-10-20 15:47:55 +0300

    Create method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.io.File;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        String folderPath = "D:/Музыка";
+        File file = new File(folderPath);
 
+        System.out.println(file.length());
+        System.out.println(); //Разделительная строка.
+
+      /*  Set keys = System.getProperties().keySet();
+        for (Object key : keys) {
+            System.out.println(key);
+        }
+        System.out.println(); //Разделительная строка.
+        System.out.println(System.getProperties().get("user.dir"));
+        */
+        System.out.println(); //Разделительная строка.
+        System.out.println(getFolderSize(file));
+    }
+
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
