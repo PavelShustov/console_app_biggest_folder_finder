55671cdc1fa4ecdd909a4d207cb8c780b843acca
Pavel Shustov <pavel.shustov777@gmail.com>
2024-10-20 18:06:02 +0300

    Realazed methods getHumanReadableSize and getSizeFromHumanReadableSize

--- src/Main.java
+++ src/Main.java
@@ -44,6 +44,10 @@ public class Main {
         long duration1 = System.currentTimeMillis() - start1;
         System.out.println(); //Разделительная строка.
         System.out.println(duration1 + "ms");
+        System.out.println(); //Разделительная строка.
+        System.out.println(getHumanReadableSize(getFolderSize(file)));
+        System.out.println(); //Разделительная строка.
+        System.out.println(getSizeFromHumanReadableSize(getHumanReadableSize(getFolderSize(file))));
     }
 
     public static long getFolderSize(File folder) {
@@ -57,5 +61,44 @@ public class Main {
             sum += getFolderSize(file);
         }
         return sum;
+    }
+
+    //TODO: 24B, 234Kb, 36Mb, 34Gb, 42Tb
+    public static String getHumanReadableSize(long size) {
+
+        if (size > 1024 && size < Math.pow(1024, 2)) {
+            size = size / 1024;
+            return size + "Kb";
+        }
+        if (size > Math.pow(1024, 2) && size < Math.pow(1024, 3) && size < Math.pow(1024, 4)) {
+            size = size / (long) Math.pow(1024, 2);
+            return size + "Mb";
+        }
+        if (size > Math.pow(1024, 3) && size < Math.pow(1024, 4)) {
+            size = size / (long) Math.pow(1024, 3);
+            return size + "Gb";
+        }
+        if (size > Math.pow(1024, 4)) {
+            size = size / (long) Math.pow(1024, 4);
+            return size + "Tb";
+        }
+        return size + "B";
+    }
+
+    //TODO: 24B, 234Kb, 36Mb, 34Gb, 42Tb
+    //  24B, 234K, 36M, 34G, 42T
+    //  234 => 239616
+    public static long getSizeFromHumanReadableSize(String size) {
+        long valueOfSize = Long.valueOf(size.replaceAll("[^0-9]+", ""));
+        if (size.contains("K")) {
+            return valueOfSize * 1024;
+        } else if (size.contains("M")) {
+            return valueOfSize * (long) Math.pow(1024, 2);
+        } else if (size.contains("G")) {
+            return valueOfSize * (long) Math.pow(1024, 3);
+        } else if (size.contains("T")) {
+            return valueOfSize * (long) Math.pow(1024, 4);
+        }
+        return valueOfSize;
     }
 }
